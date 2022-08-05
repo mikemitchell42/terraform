2207c567671a92af8bdaad4161e67a519c686d37
mikemitchell42 <110642577+mikemitchell42@users.noreply.github.com>
2022-08-04 22:38:26 -0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
  terraform {
    backend "s3" {
-    bucket = "code-sharing-bucket"
+    bucket = "vamsi-news"
     key    = "avinash/s3file-3.tfstate"
-    region = "us-west-2"
-    profile = "jespo"
+    region = "us-east-1"
+    profile = "mike"
 dynamodb_table= "firstprojecttable"
    }
  }
